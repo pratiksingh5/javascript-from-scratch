1f0bbeb78bc8fd7ddf3f687ba0758690107c4630
Pratik Singh <pratikrajaryan@gmail.com>
2023-10-11 20:53:50 +0530

    Array Basics

---
+++ Chapter_4_Array/02_AccessElement.js
@@ -0,0 +1,9 @@
+// let country = ["India", "Australia", "England", "South Africa"]
+
+let country = ["Australia", "India", "South Africa", "England"]
+
+
+// console.log(country[2])
+// console.log(country[4])  //undefined
+console.log(country[3])
+
