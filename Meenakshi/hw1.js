6d39b16dbc3309454e1f5c9f19ebc4d2a6d8ae6b
Meenakshi <mehrameenakshi1995@gmail.com>
2023-10-11 19:47:35 +0530

    hw1 file added

---
+++ Meenakshi/hw1.js
@@ -0,0 +1,9 @@
+let number=18;
+
+if(number>0){
+    console.log("not eligible to vote");
+}
+else{
+    console.log("eligible to vote");
+}
+
---
+++ Meenakshi/ifelse.js
@@ -0,0 +1,18 @@
+// Write a program that checks if a given number is positive, negative, or zero.
+let number =5;
+
+if(number>0) {
+    console.log("positive");
+}
+else if(number==0){
+    console.log("zero");
+}
+else{
+    console.log("negative");
+}
+
+
+
+
+
+
