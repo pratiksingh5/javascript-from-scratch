a661e15dd53a13b3417f6d5f3c18878ae8fe2ed1
Nishant Kumar <nishant.kode0@gmail.com>
2023-10-11 19:44:57 +0530

    homework 2 completed

---
+++ Nishant/hw2.js
@@ -0,0 +1,18 @@
+
+
+
+// Write a program that checks if a given number is positive, negative, or zero.
+
+
+
+let number =2;
+
+if(number>0){
+    console.log("The Number is positive.");
+}
+else if(number==0){
+    console.log("The number is zero.");
+}
+else{
+    console.log("The number is negative.");
+}
---
+++ Nishant/hw3.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+// Write a JavaScript program to find the largest of three numbers.
+
+
+let num1=7;
+let num2=12;
+let num3=9;
+
+if(num1>=num2 && num1>=num3){
+    console.log("num1 is largest");
+}
+else if(num2>=num1 && num2>=num3){
+    console.log("num2 is largest");
+}
+else{
+    console.log("num3 is largest")
+}
+
