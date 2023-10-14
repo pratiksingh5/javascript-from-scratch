42625928531105c0bd05d2728eec6ddd629fb2ce
Nishant Kumar <nishant.kode0@gmail.com>
2023-10-14 13:38:43 +0530

    homework completed

---
+++ homework.js/hw8.js
@@ -0,0 +1,28 @@
+// Write a program that finds the day of the week based on a number input (1 for Sunday, 2 for Monday, and so on).
+
+let number = 6;
+
+if(number==1){
+    console.log("It's Sunday today");
+}
+else if(number==2){
+    console.log("It's Monday today");
+}
+else if(number==3){
+    console.log("It's Tuesday today");
+}
+else if(number==4){
+    console.log("It's Wednesday today");
+}
+else if(number==5){
+    console.log("It's Thursday today");
+}
+else if(number==6){
+    console.log("It's Friday today");
+}
+else if(number==7){
+    console.log("It's Saturday today");
+}
+else{
+    console.log("Invalid Input");
+}
