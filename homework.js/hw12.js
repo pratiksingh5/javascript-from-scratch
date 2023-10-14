34bb65e7c9506a6cb333ae8a776a0d2b7e38add3
Nishant Kumar <nishant.kode0@gmail.com>
2023-10-14 15:22:22 +0530

    homework completed

---
+++ homework.js/hw12.js
@@ -0,0 +1,10 @@
+// Write a program that checks whether a number entered by the user is between 1 and 100 
+
+let number = 199;
+
+if(number>=1 && number<=100){
+    console.log("Number is between 1 and 100")
+}
+else{
+    console.log("Number is invalid")
+}
