a9bfbbd55c5c69555595148f7bfcc93a5a443d59
Nishant Kumar <nishant.kode0@gmail.com>
2023-10-14 15:51:24 +0530

    homework completed

---
+++ homework.js/hw13.js
@@ -0,0 +1,11 @@
+// Create a program that determines if a number entered by the user is divisible by both 3 and 5.
+
+let number = 45;
+
+if (number%15==0){
+    console.log("It is divisible by 3 and 5")
+}
+else{
+    console.log("It is not divisible")
+}
+
---
+++ homework.js/hw16.js
@@ -0,0 +1,10 @@
+// Create a program that checks if a number entered by the user is divisible by either 2 or 3.
+
+let number = 6;
+
+if (number%6==0){
+    console.log("the number is divisible by both 2 and 3")
+}
+else{
+    console.log("The number is not divisible")
+}
