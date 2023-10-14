b45c4bd5f4aec20a3d875af1ba67bdaa66be949f
Nishant Kumar <nishant.kode0@gmail.com>
2023-10-14 16:00:18 +0530

    homework completed

---
+++ homework.js/hw14.js
@@ -0,0 +1,18 @@
+// write a program that asks the user for their age and checks if they are eligible for both voting (age >= 18) and driving (age >= 16).
+
+
+let age = 17;
+
+if (age>=18){
+    console.log("Eligible for voting")
+}
+else{
+    console.log("Not Eligible for voting")
+}
+
+if(age>=16){
+    console.log("Eligiblr for driving")
+}
+else{
+    console.log("Not Eligible for driving")
+}
---
+++ homework.js/hw15.js
@@ -0,0 +1,22 @@
+// Write a program that determines whether a number entered by the user is positive and odd
+
+let number = -8;
+
+
+
+if (number > 0){
+    console.log("The given number is positive")
+}
+else if (number == 0){
+    console.log("The given number is zero")
+}
+else{
+    console.log("The given number is negative")
+}
+
+if(number%2==0){
+    console.log("and even")
+}
+else{
+    console.log("and odd")
+}
