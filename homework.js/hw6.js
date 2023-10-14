12986c7ce34ab0d6b9ca9f6cbd3423af51a570f9
Nishant Kumar <nishant.kode0@gmail.com>
2023-10-14 13:25:47 +0530

    homework completed

---
+++ homework.js/hw6.js
@@ -0,0 +1,10 @@
+// Write a program that checks if a character entered by the user is a vowel or a consonant.
+
+let input = "b";
+
+if(input === "a" || input === "e" || input === "i" || input === "o" || input === "u"){
+    console.log("Vowel")
+}
+else{
+    console.log("Consonant")
+}
