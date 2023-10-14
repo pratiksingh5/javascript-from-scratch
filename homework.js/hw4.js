6ca6989474f0c8d0458884765084e4c82854b2e2
Nishant Kumar <nishant.kode0@gmail.com>
2023-10-14 13:11:28 +0530

    homework completed

---
+++ homework.js/hw4.js
@@ -0,0 +1,10 @@
+// Create a program that checks if a person is eligible to vote based on their age.
+
+let number=19;
+
+if(number>=18){
+    console.log("You are eligible to vote.");
+}
+else{
+    console.log("You are not eligible to vote.");
+}
