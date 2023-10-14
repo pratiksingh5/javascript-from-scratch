bac6f5d1dc8b7eec86b639a35de0067b13c61a8f
Nishant Kumar Singh <125122013+NishantKodes01@users.noreply.github.com>
2023-10-14 16:04:14 +0530

    homework hw5.js

--- homework.js/hw5.js
+++ homework.js/hw5.js
@@ -1,11 +1,2 @@
 // Create a program that calculates the total cost of purchasing a product. If the quantity purchased is more than 10, apply a 10% discount; otherwise, no discount is applied.
 
-let number =10;
-
-if(number>=10){
-    discount=(10/100*100);
-    console.log("Discount applied");
-}
-else{
-    console.log("Discount not applied");
-}
